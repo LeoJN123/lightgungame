6127342c4b4da4e83022369b39511079140d6d29
Leo Nikander <leo.nikander@laajasalonopisto.fi>
2017-06-16 12:58:13 +0300

    Gun should now work

--- Lightgun Game/Assets/Scripts/Blaster.cs
+++ Lightgun Game/Assets/Scripts/Blaster.cs
@@ -1,114 +1,30 @@
-﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
-//
-// Purpose: Demonstrates how to create a simple interactable object
-//
-//=============================================================================
-
-using UnityEngine;
+﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
 
-namespace Valve.VR.InteractionSystem {
-    //-------------------------------------------------------------------------
-    [RequireComponent(typeof(Interactable))]
-    public class Blaster : MonoBehaviour {
-        private TextMesh textMesh;
-        private Vector3 oldPosition;
-        private Quaternion oldRotation;
+public class Blaster : MonoBehaviour {
 
-        private float attachTime;
+    public GameObject muzzle;
+    public GameObject bullet;
 
-        private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers);
+    private void HandAttachedUpdate (Hand hand) {
 
-        //-------------------------------------------------
-        void Awake() {
-            textMesh = GetComponentInChildren<TextMesh>();
-            textMesh.text = "No Hand Hovering";
+#if UNITY_EDITOR
+        if (hand.controller == null && hand.GetStandardInteractionButtonDown()) {//debug shoot
+            Shoot();
+            return;
         }
+#endif
 
-
-        //-------------------------------------------------
-        // Called when a Hand starts hovering over this object
-        //-------------------------------------------------
-        private void OnHandHoverBegin(Hand hand) {
-            textMesh.text = "Hovering hand: " + hand.name;
+        if (hand.controller != null && hand.controller.GetHairTriggerDown()) {
+            Shoot();
         }
+    }
 
-
-        //-------------------------------------------------
-        // Called when a Hand stops hovering over this object
-        //-------------------------------------------------
-        private void OnHandHoverEnd(Hand hand) {
-            textMesh.text = "No Hand Hovering";
-        }
-
-
-        //-------------------------------------------------
-        // Called every Update() while a Hand is hovering over this object
-        //-------------------------------------------------
-        private void HandHoverUpdate(Hand hand) {
-            if (hand.GetStandardInteractionButtonDown() || ((hand.controller != null) && hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip))) {
-                if (hand.currentAttachedObject != gameObject) {
-                    // Save our position/rotation so that we can restore it when we detach
-                    oldPosition = transform.position;
-                    oldRotation = transform.rotation;
-
-                    // Call this to continue receiving HandHoverUpdate messages,
-                    // and prevent the hand from hovering over anything else
-                    hand.HoverLock(GetComponent<Interactable>());
-
-                    // Attach this object to the hand
-                    hand.AttachObject(gameObject, attachmentFlags);
-                } else {
-                    // Detach this object from the hand
-                    hand.DetachObject(gameObject);
-
-                    // Call this to undo HoverLock
-                    hand.HoverUnlock(GetComponent<Interactable>());
-
-                    // Restore position/rotation
-                    transform.position = oldPosition;
-                    transform.rotation = oldRotation;
-                }
-            }
-        }
-
-
-        //-------------------------------------------------
-        // Called when this GameObject becomes attached to the hand
-        //-------------------------------------------------
-        private void OnAttachedToHand(Hand hand) {
-            textMesh.text = "Attached to hand: " + hand.name;
-            attachTime = Time.time;
-        }
-
-
-        //-------------------------------------------------
-        // Called when this GameObject is detached from the hand
-        //-------------------------------------------------
-        private void OnDetachedFromHand(Hand hand) {
-            textMesh.text = "Detached from hand: " + hand.name;
-        }
-
-
-        //-------------------------------------------------
-        // Called every Update() while this GameObject is attached to the hand
-        //-------------------------------------------------
-        private void HandAttachedUpdate(Hand hand) {
-            textMesh.text = "Attached to hand: " + hand.name + "\nAttached time: " + (Time.time - attachTime).ToString("F2");
-        }
-
-
-        //-------------------------------------------------
-        // Called when this attached GameObject becomes the primary attached object
-        //-------------------------------------------------
-        private void OnHandFocusAcquired(Hand hand) {
-        }
-
-
-        //-------------------------------------------------
-        // Called when another attached GameObject becomes the primary attached object
-        //-------------------------------------------------
-        private void OnHandFocusLost(Hand hand) {
-        }
+    private void Shoot () {
+        Instantiate(bullet, muzzle.transform.position, muzzle.transform.rotation);
     }
 }
---
+++ Lightgun Game/Assets/Scripts/Blasterbullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blasterbullet : MonoBehaviour {
+
+    [SerializeField] float bulletSpeed;
+    [SerializeField] float lifeTime;
+
+    private void Start() {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Update() {
+        transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+    }
+}
