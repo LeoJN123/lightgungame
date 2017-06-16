29fe4fc5e9c84d7e24aa8e5ecc61a1175c063a6e
Leo Nikander <leo.nikander@laajasalonopisto.fi>
2017-06-16 10:52:15 +0300

    gummallisuuksia

---
+++ Lightgun Game/Assets/Scripts/Package Scripts/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Hand hand1, hand2;
+    bool slowdown = false;
+    public float slowDownScale = 0.5f;
+
+	void Update ()
+    {
+        if ((hand1.controller != null && hand1.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip)) || (hand2.controller != null && hand2.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip)) || Input.GetKeyDown(KeyCode.Space)) 
+        {
+            slowdown = !slowdown;
+            Time.timeScale = slowdown ? slowDownScale : 1f;
+        }
+
+	}
+}
---
+++ Lightgun Game/Assets/Scripts/Package Scripts/RemoveChildren.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveChildren : MonoBehaviour
+{
+    public void Remove()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+}
