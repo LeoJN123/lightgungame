c163bd5c3638132b1652adba51057f9885b15d54
Leo Nikander <leo.nikander@laajasalonopisto.fi>
2017-06-19 13:09:02 +0300

    progress

---
+++ Lightgun Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public enum enemyTier {blue, red, pink};
+
+    public enemyTier enemyLevel;
+    public float speed;
+
+    public GameObject player;
+    Vector3 direction;
+    Vector3 playerLocation;
+    Rigidbody rb;
+
+    //Ignore these
+    
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+
+        var v = rb.velocity;
+        var pp = player.transform.position;
+        var d = (pp - transform.position).normalized;
+        v = v + d * speed * Time.fixedDeltaTime;
+        rb.velocity = v;
+
+        if (enemyLevel == enemyTier.blue) {
+
+        }
+
+        if (enemyLevel == enemyTier.red) {
+
+        }
+
+        if (enemyLevel == enemyTier.pink) {
+
+        }
+    }
+}
---
+++ Lightgun Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject enemyContainer;
+    public GameObject[] spawnPoints;
+    public Vector3 spawnerMovementV3;
+    public float spawnerMovementSpeed;
+
+    public GameObject[] enemies;
+    public float spawnTime = 2f;
+    [Tooltip("Number between 0 and 1")]
+    public float difficultyPercentage = 0.95f;
+
+    private void Start() {
+        GameStart();
+    }   
+
+    public void GameStart () {
+        StartCoroutine(Spawning());
+    }
+
+    IEnumerator Spawning () {
+
+        while (true) {
+            InstantiateEnemy();
+            yield return new WaitForSeconds(spawnTime);
+            spawnTime *= difficultyPercentage;
+            
+        }
+    }
+
+    void InstantiateEnemy() {
+
+        GameObject enemy = enemies[Random.Range(0, enemies.Length)];
+        GameObject spawn = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        Instantiate(enemy, spawn.transform.position, spawn.transform.rotation, enemyContainer.transform);
+    }
+
+    private void Update() {
+        /* Rotating the spawner like a carousel */
+        transform.Rotate(spawnerMovementV3 * spawnerMovementSpeed * Time.deltaTime);
+    }
+}
