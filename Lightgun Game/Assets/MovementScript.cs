c1e77f9118e1b262cdff77353aff7c1d5cda5d90
OPISTO\roni.sääski <roni.saaski@laajasalonopisto.fi>
2017-06-21 13:53:29 +0300

    MovementScript lisätty, testaamatta

---
+++ Lightgun Game/Assets/MovementScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
+
+public class MovementScript : MonoBehaviour
+{
+    public LayerMask floor;
+    public LayerMask enemy;
+    public GameObject player;
+    // Update is called once per frame
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, fwd, out hit))
+            Debug.DrawLine(transform.position, hit.point);
+
+
+    }
+    private void HandAttachedUpdate(Hand hand)
+    {
+        RaycastHit hit;
+        Vector3 dwn = transform.TransformDirection(Vector3.down);
+        if (hand.controller != null && hand.controller.GetHairTriggerDown() && Physics.Raycast(transform.position, dwn, out hit))
+        {
+            player.transform.Translate(hit.point);
+        }
+    }
+}
