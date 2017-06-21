4411782d7fef0a7c67ba2a77cfe90bf141325250
Leo Nikander <leo.nikander@laajasalonopisto.fi>
2017-06-21 10:58:39 +0300

    kilpi toimii ja siin on kivoi juttui

---
+++ Lightgun Game/Assets/Scripts/Shield.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Shield : MonoBehaviour {
+
+    public UnityEvent shieldHit;
+
+    private void OnTriggerEnter(Collider other) {
+        print("Shield hit something!");
+        shieldHit.Invoke();
+    }
+
+}
